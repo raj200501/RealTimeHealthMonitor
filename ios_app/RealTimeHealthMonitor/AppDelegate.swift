066e63500fbc7cc71be1ca924715adfbf4e17c6f
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-10 23:34:48 -0700

    Create AppDelegate.swift

---
+++ ios_app/RealTimeHealthMonitor/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SwiftUI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let contentView = ContentView()
+
+        if let windowScene = UIWindowScene(sessionRole: .windowApplication) {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
